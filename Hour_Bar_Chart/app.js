50f7f20049ba13cda868063af951bf2c97c8429e
Alexander Gonzalez <gonzalexander294@gmail.com>
2021-05-31 16:54:18 -0400

    First commit

---
+++ Hour_Bar_Chart/app.js
@@ -0,0 +1,76 @@
+// Define SVG area dimensions
+var svgWidth =960;
+var svgHeight = 860;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 60,
+  right: 15,
+  bottom: 50,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from count-of-tv-watched.csv
+d3.csv("count_df.csv").then(function(hourData) {
+
+  console.log(hourData);
+
+  // Cast the count value to a number for each piece of hourData
+  hourData.forEach(function(d) {
+    d.count = +d.count;
+  });
+
+  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+  var xBandScale = d3.scaleBand()
+    .domain(hourData.map(d => d.hour))
+    .range([0, chartWidth])
+    .padding(0.1);
+
+  // Create a linear scale for the vertical axis.
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(hourData, d => d.count)])
+    .range([chartHeight, 0]);
+
+  // Create two new functions passing our scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xBandScale);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+
+  // Create one SVG rectangle per piece of hourData
+  // Use the linear and band scales to position each rectangle within the chart
+  chartGroup.selectAll(".bar")
+    .data(hourData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => xBandScale(d.hour))
+    .attr("y", d => yLinearScale(d.count))
+    .attr("width", xBandScale.bandwidth())
+    .attr("height", d => chartHeight - yLinearScale(d.count));
+
+}).catch(function(error) {
+  console.log(error);
+});
